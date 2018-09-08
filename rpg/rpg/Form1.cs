33ebab54d4d0d480b2ddda4a4a9adfb403d71642
liuliu.home <193002818@qq.com>
2018-09-09 07:45:31 +0800

    提取角色类Player.cs

--- rpg/rpg/Form1.cs
+++ rpg/rpg/Form1.cs
@@ -10,12 +10,12 @@ namespace rpg
 {
     public partial class Form1 : Form
     {
-        int x = 50, y = 50;
-        int face = 1;
+        Player player = new Player();
         int animation_ctrl = 0;
         public Form1()
         {
             InitializeComponent();
+            Draw();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -25,42 +25,22 @@ namespace rpg
 
         private void Draw()
         {
-            Bitmap bitmap = new Bitmap(ResourceContextDeterminer.GetAssetPath("r1.png"));
-            bitmap.SetResolution(96, 96);
             Graphics g1 = pictureBox1.CreateGraphics();
             BufferedGraphicsContext currentContext = BufferedGraphicsManager.Current;
             BufferedGraphics myBuffer = currentContext.Allocate(g1, this.DisplayRectangle);
             Graphics g = myBuffer.Graphics;
             animation_ctrl += 1;
-            Rectangle crazycoderRgl = new Rectangle(bitmap.Width/4*(animation_ctrl%4), bitmap.Height/4*(face-1), bitmap.Width/4, bitmap.Height/4);
-            Bitmap bitmap0 = bitmap.Clone(crazycoderRgl, bitmap.PixelFormat);
-            g.DrawImage(bitmap0, x, y);
+            player.draw(g, animation_ctrl);
             myBuffer.Render();
             myBuffer.Dispose();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            Console.WriteLine("for1.keydown");
+            Console.WriteLine("form1.keydown");
 
             pictureBox1.Refresh();
-            if (e.KeyCode == Keys.Up)
-            {
-                y -= 5;
-                face = 4;
-            } else if (e.KeyCode ==  Keys.Down)
-            {
-                y += 5;
-                face = 1;
-            } else if (e.KeyCode == Keys.Left)
-            {
-                x -= 5;
-                face = 2;
-            } else if (e.KeyCode == Keys.Right)
-            {
-                x += 5;
-                face = 3;
-            }
+            player.key_ctrl(e);
             Draw();
         }
     }
---
+++ rpg/rpg/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rpg
+{
+    class Player
+    {
+        int x = 50;
+        int y = 50;
+        int face = 1;
+        Bitmap bitmap;
+
+        public Player()
+        {
+            bitmap = new Bitmap(ResourceContextDeterminer.GetAssetPath("r1.png"));
+            bitmap.SetResolution(96, 96);
+        }
+
+        public void key_ctrl(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                y -= 5;
+                face = 4;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                y += 5;
+                face = 1;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                x -= 5;
+                face = 2;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                x += 5;
+                face = 3;
+            }
+        }
+
+        public void draw(Graphics g, int animation_ctrl) 
+        {
+            Rectangle crazycoderRgl = new Rectangle(bitmap.Width / 4 * (animation_ctrl % 4), bitmap.Height / 4 * (face - 1), bitmap.Width / 4, bitmap.Height / 4);
+            Bitmap bitmap0 = bitmap.Clone(crazycoderRgl, bitmap.PixelFormat);
+            g.DrawImage(bitmap0, x, y);
+        }
+    }
+}
